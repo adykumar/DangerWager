48d4538e8d08c48d32ff6e71363f12c007dd98ec
adykumar <adykumar01@gmail.com>
2019-08-02 19:26:03 -0700

    98 lc done

---
+++ Swad/98_LC_med_Validate_Binary_Search_Tree.py
@@ -0,0 +1,59 @@
+"""
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+"""
+
+"""
+Time: O(n)
+Space: O(1)
+"""
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+
+    def helper(self, root, lmax, rmax):
+        if root==None: return True
+        if root.val>lmax and root.val<rmax:
+            return self.helper(root.left, lmax, root.val) and self.helper(root.right, root.val, rmax)
+        return False
+
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.helper(root, -float('inf'), float('inf') )

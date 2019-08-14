fe4da4b1c3ebb45a89dfdbeee5a04de986163335
adykumar <adykumar01@gmail.com>
2019-08-14 16:34:02 -0700

    34 lc easy done

---
+++ Swad/034-100-LC-easy-SameTree.py
@@ -0,0 +1,59 @@
+"""
+100. Same Tree
+Easy
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+"""
+
+"""
+Time- O(min(p, q))  : we check every node in p and q
+Space- O(1)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p==None and q==None: return True
+        if p==None or q==None: return False
+        return p.val==q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)

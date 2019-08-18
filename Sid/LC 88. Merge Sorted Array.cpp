8f300b2596cf0eea0eca4cc6226a0e16c815b717
SAHOO-SIDDHARTH-SAHOO <sahoosiddharth3@gmail.com>
2019-08-19 00:23:45 +0530

    LC 88. Merge Sorted Array (Easy)
    LC 88. Merge Sorted Array (Easy)

---
+++ Sid/LC 88. Merge Sorted Array.cpp
@@ -0,0 +1,26 @@
+/*
+88. Merge Sorted Array
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Note:
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+*/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        
+        for(int i=m;i<nums1.size();i++){
+            nums1[i]=nums2[i-m];
+        }
+        
+        sort(nums1.begin(),nums1.end());
+        
+    }
+};

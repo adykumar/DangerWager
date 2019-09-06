6f974f5286d5fc3bd80153f1a0cf773ebf19f92a
adykumar <adykumar01@gmail.com>
2019-09-05 23:17:58 -0700

    349 lc easy

---
+++ Swad/049-349-LC-easy-IntersectionOfTwoArrays.py
@@ -0,0 +1,47 @@
+"""
+349. Intersection of Two Arrays
+Easy
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+"""
+
+"""
+Time- O(m log m + n log n)  m: length of m, n: length of n
+Space- storing intersections
+"""
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1= sorted(nums1)
+        nums2= sorted(nums2)
+        i=0
+        j=0
+        res= []
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                if res==[] or nums1[i] != res[-1]:
+                    res.append(nums1[i])
+                i+=1; j+=1
+        return res
